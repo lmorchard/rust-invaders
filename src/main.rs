d2e66d78fd2fd2e4a46fb71f84eec269a8190387
Les Orchard <me@lmorchard.com>
2018-01-08 01:16:51 -0500

    Try out specs as ECS in ggez

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,30 @@
 extern crate invaders;
 extern crate ggez;
+extern crate specs;
+#[macro_use]
+extern crate specs_derive;
 
+use std::f32::consts::PI;
+use std::time::{
+    SystemTime,
+};
 use ggez::*;
 use ggez::graphics::{
-    BlendMode,
-    Drawable,
     DrawMode,
     DrawParam,
     Mesh,
     MeshBuilder,
     Point2,
+};
+use specs::{
+    Dispatcher,
+    DispatcherBuilder,
+    Fetch,
+    Join,
+    ReadStorage,
+    System,
+    World,
+    WriteStorage,
 };
 
 // TODO: Figure out if there's a better way to write this macro
@@ -23,91 +38,167 @@ macro_rules! points {
     };
 }
 
-struct MainState {
-    pos_x: f32,
-    player_sprite: PlayerSprite,
+#[derive(Debug)]
+struct DeltaTime(pub f32);
+
+#[derive(Component, Debug)]
+struct Position { x: f32, y: f32, r: f32, }
+
+#[derive(Component, Debug)]
+struct Velocity { x: f32, y: f32, r: f32, }
+
+#[derive(Component, Debug)]
+struct Sprite {
+    scale: Point2,
+    offset: Point2,
+    mesh: Mesh,
 }
 
-impl MainState {
-    fn new(ctx: &mut Context) -> GameResult<MainState> {
-        ctx.print_resource_stats();
-        graphics::set_background_color(ctx, (0, 0, 0, 255).into());
+struct MotionSystem;
 
-        let s = MainState {
-            pos_x: 0.0,
-            player_sprite: PlayerSprite::new(ctx)
-        };
+impl<'a> System<'a> for MotionSystem {
+    type SystemData = (
+        Fetch<'a, DeltaTime>,
+        ReadStorage<'a, Velocity>,
+        WriteStorage<'a, Position>
+    );
 
-        Ok(s)
+    fn run(&mut self, data: Self::SystemData) {
+        let (delta, vel, mut pos) = data;
+        let delta = delta.0;
+        for (vel, pos) in (&vel, &mut pos).join() {
+            pos.x += vel.x * delta;
+            pos.y += vel.y * delta;
+            pos.r += vel.r * delta;
+        }
     }
 }
 
-impl event::EventHandler for MainState {
+struct MainState<'a, 'b> {
+    last_time: SystemTime,
+    world: World,
+    dispatcher: Dispatcher<'a, 'b>,
+}
+
+impl<'a, 'b> MainState<'a, 'b> {
+    fn new(ctx: &mut Context) -> GameResult<MainState<'a, 'b>> {
+        let mut world = World::new();
+
+        world.add_resource(DeltaTime(0.016));
+        world.register::<Position>();
+        world.register::<Velocity>();
+        world.register::<Sprite>();
+
+        world.create_entity()
+            .with(Position { x: 4.0, y: 200.0, r: 0.0 })
+            .with(Velocity { x: 100.0, y: 0.0, r: PI / 2.0 })
+            .with(Sprite {
+                mesh: build_player_mesh(ctx, 0.01),
+                offset: Point2::new(0.5, 0.5),
+                scale: Point2::new(100.0, 100.0),
+            })
+            .build();
+
+        world.create_entity()
+            .with(Position { x: 4.0, y: 300.0, r: 0.0 })
+            .with(Velocity { x: 150.0, y: 0.0, r: PI / 3.0 })
+            .with(Sprite {
+                mesh: build_player_mesh(ctx, 0.02),
+                offset: Point2::new(0.5, 0.5),
+                scale: Point2::new(50.0, 50.0),
+            })
+            .build();
+
+        world.create_entity()
+            .with(Position { x: 4.0, y: 100.0, r: 0.0 })
+            .with(Velocity { x: 50.0, y: 0.0, r: PI / 3.0 })
+            .with(Sprite {
+                mesh: build_player_mesh(ctx, 0.02),
+                offset: Point2::new(0.5, 0.5),
+                scale: Point2::new(50.0, 50.0),
+            })
+            .build();
+
+        let dispatcher = DispatcherBuilder::new()
+            .add(MotionSystem, "motion", &[])
+            .build();
+
+        Ok(MainState {
+            world,
+            dispatcher,
+            last_time: SystemTime::now(),
+        })
+    }
+}
+
+impl<'a, 'b> event::EventHandler for MainState<'a, 'b> {
     fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
+        let now = SystemTime::now();
+        let dt = now.duration_since(self.last_time).unwrap();
+        self.last_time = now;
+
+        {
+            let mut delta = self.world.write_resource::<DeltaTime>();
+            *delta = DeltaTime(dt.as_secs() as f32 + dt.subsec_nanos() as f32 * 1e-9);
+        }
+
+        self.dispatcher.dispatch(&mut self.world.res);
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx);
 
-        graphics::draw_ex(ctx, &self.player_sprite, DrawParam {
-            dest: Point2::new(self.pos_x, 200.0),
-            rotation: self.pos_x / 40.0,
-            offset: Point2::new(0.5, 0.5),
-            scale: Point2::new(100.0, 100.0),
-            .. Default::default()
-        })?;
+        let entities = self.world.entities();
+        let positions = self.world.read::<Position>();
+        let sprites = self.world.read::<Sprite>();
+
+        for (_ent, pos, spr) in (&*entities, &positions, &sprites).join() {
+            graphics::draw_ex(ctx, &spr.mesh, DrawParam {
+                dest: Point2::new(pos.x, pos.y),
+                rotation: pos.r,
+                offset: spr.offset,
+                scale: spr.scale,
+                .. Default::default()
+            })?;
+        }
 
         graphics::present(ctx);
         Ok(())
     }
 }
 
-struct PlayerSprite {
-    shape: Mesh
-}
-
-impl PlayerSprite {
-    fn new(ctx: &mut Context) -> Self {
-        let shape = MeshBuilder::new()
-            .polygon(DrawMode::Line(0.01), &points![
-                (0.0, 0.0),
-                (1.0, 0.0),
-                (1.0, 1.0),
-                (0.0, 1.0)
-            ])
-            .polygon(DrawMode::Line(0.01), &points![
-                (0.5, 0.0),
-                (1.0, 1.0),
-                (0.0, 1.0)
-            ])
-            .line(&points![
-                (0.4, 0.5),
-                (0.6, 0.5)
-            ], 0.01)
-            .line(&points![
-                (0.5, 0.4),
-                (0.5, 0.6)
-            ], 0.01)
-            .build(ctx)
-            .unwrap();
-
-        PlayerSprite { shape }
-    }
-}
-
-impl Drawable for PlayerSprite {
-    fn get_blend_mode(&self) -> Option<BlendMode> { Some(BlendMode::Add) }
-    fn set_blend_mode(&mut self, _mode: Option<BlendMode>) { }
-    fn draw_ex(&self, ctx: &mut Context, param: DrawParam) -> GameResult<()> {
-        graphics::draw_ex(ctx, &self.shape, param)
-    }
+fn build_player_mesh (ctx: &mut Context, line_width: f32) -> Mesh {
+    MeshBuilder::new()
+        .polygon(DrawMode::Line(line_width), &points![
+            (0.0, 0.0),
+            (1.0, 0.0),
+            (1.0, 1.0),
+            (0.0, 1.0)
+        ])
+        .polygon(DrawMode::Line(line_width), &points![
+            (0.5, 0.0),
+            (1.0, 1.0),
+            (0.0, 1.0)
+        ])
+        .line(&points![
+            (0.4, 0.5),
+            (0.6, 0.5)
+        ], line_width)
+        .line(&points![
+            (0.5, 0.4),
+            (0.5, 0.6)
+        ], line_width)
+        .build(ctx)
+        .unwrap()
 }
 
 pub fn main() {
     let c = conf::Conf::new();
     let ctx = &mut Context::load_from_conf("super_simple", "ggez", c).unwrap();
+
+    ctx.print_resource_stats();
+    graphics::set_background_color(ctx, (0, 0, 0, 255).into());
 
     let state = &mut MainState::new(ctx).unwrap();
     event::run(ctx, state).unwrap();
