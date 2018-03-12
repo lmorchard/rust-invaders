9f9896b5dafefab2b23f9b01fcfe4ec0e4ce380b
Les Orchard <me@lmorchard.com>
2018-03-12 00:33:34 -0400

    Refactor hud gauges to DRY things out

--- src/game.rs
+++ src/game.rs
@@ -38,74 +38,82 @@ pub fn update(world: &mut World) -> GameResult<()> {
     if rand::random::<f32>() < asteroid_spawn_chance {
         spawn_asteroid(world);
     }
-
     Ok(())
 }
 
 pub fn draw(world: &mut World, font: &mut fonts::Font, ctx: &mut Context) -> GameResult<()> {
     let scale = 50.0;
-    let perc_scale = 4.0;
     let base_x = 0.0 - (viewport::PLAYFIELD_WIDTH / 2.0) + (scale * 1.0);
     let base_y = 0.0 - (viewport::PLAYFIELD_HEIGHT / 2.0) + (scale * 0.5) + 4.0;
+    let planet_icon = sprites::Shape::PlanetIcon.build_mesh(ctx, 1.0 / scale);
+    let player_icon = sprites::Shape::Player.build_mesh(ctx, 1.0 / scale);
 
-    let planets = world.read::<HeroPlanet>();
-    let planet_healths = world.read::<health_damage::Health>();
-
-    for (health, planet) in (&planet_healths, &planets).join() {
-        let perc = 100.0 * (health.health / health.max_health);
-        let mesh = sprites::Shape::PlanetIcon.build_mesh(ctx, 1.0 / scale);
-        graphics::draw_ex(
+    for (health, _planet) in (
+        &world.read::<health_damage::Health>(),
+        &world.read::<HeroPlanet>(),
+    ).join()
+    {
+        draw_hud_gauge(
             ctx,
-            &mesh,
-            DrawParam {
-                dest: Point2::new(base_x, base_y),
-                rotation: 0.0,
-                offset: Point2::new(0.5, 0.5),
-                scale: Point2::new(scale, scale),
-                ..Default::default()
-            },
-        )?;
-        graphics::rectangle(
-            ctx,
-            graphics::DrawMode::Line(1.0),
-            Rect::new(
-                base_x + (scale * 1.125),
-                base_y - (scale * 0.5),
-                perc_scale * perc,
-                scale,
-            ),
+            scale,
+            base_x,
+            base_y,
+            font,
+            &planet_icon,
+            100.0 * (health.health / health.max_health),
         )?;
     }
 
-    let players = world.read::<HeroPlayer>();
-    let player_healths = world.read::<health_damage::Health>();
-
-    for (health, player) in (&player_healths, &players).join() {
-        let perc = 100.0 * (health.health / health.max_health);
-        let mesh = sprites::Shape::Player.build_mesh(ctx, 1.0 / scale);
-        graphics::draw_ex(
+    for (health, _player) in (
+        &world.read::<health_damage::Health>(),
+        &world.read::<HeroPlayer>(),
+    ).join()
+    {
+        draw_hud_gauge(
             ctx,
-            &mesh,
-            DrawParam {
-                dest: Point2::new(base_x, base_y + (scale * 1.5)),
-                rotation: 0.0,
-                offset: Point2::new(0.5, 0.5),
-                scale: Point2::new(scale, scale),
-                ..Default::default()
-            },
-        )?;
-        graphics::rectangle(
-            ctx,
-            graphics::DrawMode::Line(1.0),
-            Rect::new(
-                base_x + (scale * 1.125),
-                base_y - (scale * 0.5) + (scale * 1.5),
-                perc_scale * perc,
-                scale,
-            ),
+            scale,
+            base_x,
+            base_y + (scale * 1.5),
+            font,
+            &player_icon,
+            100.0 * (health.health / health.max_health),
         )?;
     }
 
+    Ok(())
+}
+
+pub fn draw_hud_gauge(
+    ctx: &mut Context,
+    scale: f32,
+    base_x: f32,
+    base_y: f32,
+    font: &mut fonts::Font,
+    icon: &Mesh,
+    perc: f32,
+) -> GameResult<()> {
+    let perc_scale = 4.0;
+    graphics::draw_ex(
+        ctx,
+        &*icon,
+        DrawParam {
+            dest: Point2::new(base_x, base_y),
+            rotation: 0.0,
+            offset: Point2::new(0.5, 0.5),
+            scale: Point2::new(scale, scale),
+            ..Default::default()
+        },
+    )?;
+    graphics::rectangle(
+        ctx,
+        graphics::DrawMode::Line(1.0),
+        Rect::new(
+            base_x + (scale * 1.125),
+            base_y - (scale * 0.5),
+            perc_scale * perc,
+            scale,
+        ),
+    )?;
     Ok(())
 }
 
